e59ce8557f3fd2897398460bf597094559a47a88
Matt Hesketh <matt@heskethwebdesign.co.uk>
2026-03-10 00:31:14 +0000

    feat(terminal): replace dual terminals with unified termlib emulator

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.serverdash.app"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "com.serverdash.app.HiltTestRunner"
     }
